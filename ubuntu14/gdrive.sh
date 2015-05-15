fa8dcbf3cf62d91a88a600d91d22822ff3167635
Dmitry Gelfer <dmitry@feedvisor.com>
2015-05-15 16:35:11 +0300

    first commit

---
+++ ubuntu14/gdrive.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+add-apt-repository ppa:thefanclub/grive-tools
+apt-get update
+apt-get install grive-tools
---
+++ ubuntu14/gnome-encfs-manager.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+add-apt-repository ppa:gencfsm
+apt-get update
+apt-get -y install gnome-encfs-manager

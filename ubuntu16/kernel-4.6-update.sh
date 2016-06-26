d5e81879d48b772c55d7138daf9424e059f9fca4
geldim <geldim@users.noreply.github.com>
2016-06-26 20:27:37 +0300

    ubuntu16 kernel update - change docker filesystem driver to overlayfs due to aufs incompatibility with new kernel

--- ubuntu16/kernel-4.6-update.sh
+++ ubuntu16/kernel-4.6-update.sh
@@ -9,3 +9,15 @@ wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.6.2-yakkety/linux-headers-
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.6.2-yakkety/linux-image-4.6.2-040602-generic_4.6.2-040602.201606100516_amd64.deb
 
 sudo dpkg -i *.deb
+
+systemctl stop docker
+ 
+CONFIGURATION_FILE=$(systemctl show --property=FragmentPath docker | cut -f2 -d=)
+cp $CONFIGURATION_FILE /etc/systemd/system/docker.service
+ 
+perl -pi -e 's/^(ExecStart=.+)$/$1 -s overlay/' /etc/systemd/system/docker.service
+ 
+systemctl daemon-reload
+systemctl start docker
+
+echo "reboot machine now"
